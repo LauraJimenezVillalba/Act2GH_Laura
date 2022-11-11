4e5f5a3681de3b84b2c469740e89ccddf01ee347
Laura Jiménez Villalba <a_laura.jimenez.villalba@iespablopicasso.es>
2022-11-11 13:35:18 -0100

    Añadir contador

---
+++ NumerosAleatoriosPrint.java
@@ -0,0 +1,26 @@
+public class NumerosAleatorios {
+
+  public static void main(String[] args) {
+    final int LONGITUDNUMEROS = 20;
+    final int MINIMO = 1;
+    final int MAXIMO = 10;
+    int[] numerosAleatorios = new int[LONGITUDNUMEROS];
+    for (int i = 0; i < LONGITUDNUMEROS; i++) {
+      numerosAleatorios[i] = (int)(Math.random() * (MAXIMO - MINIMO + 1) + MINIMO);
+      }
+    for (int i = 0; i < LONGITUDNUMEROS; i++) {
+      System.out.println("numerosAleatorios[" + i + "] = " + numerosAleatorios[i]);
+    }
+    int[] vecesRepetidas = new int[MAXIMO];
+    for (int j = 0; j < MAXIMO; j++) {
+      for (int k = 0; k < LONGITUDNUMEROS; k++) {
+        if (numerosAleatorios[j] == k) {
+          vecesRepetidas[j]++;
+        }
+      }
+      System.out.print("El número " + (j + 1) + " se repite " + vecesRepetidas[j] + " veces." );
+    }
+    
+  }
+
+}
