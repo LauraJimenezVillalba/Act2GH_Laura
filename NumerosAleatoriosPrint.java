aa9e29b21b38532b876f208ccbadc95c0f4a67a7
Laura Jiménez Villalba <a_laura.jimenez.villalba@iespablopicasso.es>
2022-11-11 13:49:44 -0100

    Cambiado de 20 a 30

---
+++ NumerosAleatoriosPrint.java
@@ -0,0 +1,15 @@
+public class NumerosAleatorios {
+
+  public static void main(String[] args) {
+    final int LONGITUDNUMEROS = 30;
+    final int MINIMO = 1;
+    final int MAXIMO = 10;
+    int[] numerosAleatorios = new int[LONGITUDNUMEROS];
+    for (int i = 0; i < LONGITUDNUMEROS; i++) {
+      numerosAleatorios[i] = (int)(Math.random() * (MAXIMO - MINIMO + 1) + MINIMO);
+
+  }
+    
+  }
+
+}
